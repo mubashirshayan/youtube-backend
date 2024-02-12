8d8b415bb867c0d5b53625509aa3c60f4dcf4de0
mubashirshayan <shayanraees890@gmail.com>
2024-02-12 23:04:46 +0500

    add last change

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello server")
+import express from "express"
+import { Router } from "express";
+const app=express();
+
+
+
+const PORT=5001;
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+app.listen(PORT,()=>{
+    console.log(`server is running on PORT ${PORT}`)
+})
