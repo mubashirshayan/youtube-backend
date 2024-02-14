e1eea65f36e76dd3b2e1a7b7388e8a4e71faa81d
mubashirshayan <shayanraees890@gmail.com>
2024-02-14 22:48:33 +0500

    massive change

--- database/index.js
+++ database/index.js
@@ -1,2 +1,3 @@
 import mongoose from "mongoose";
-mongoose.connect()
+mongoose.connect('mongodb+srv://mubashirRaees:mubashir810@youtube-backend.3v66tg3.mongodb.net/?retryWrites=true&w=majority')
+export default mongoose;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { Router } from "express";
+
 const app=express();
 
 
