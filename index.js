068a979089e5517f7f6cb9eda620d39eb195ddef
mubashirshayan <shayanraees890@gmail.com>
2024-02-14 22:56:19 +0500

    changes indexfile

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express"
 import { Router } from "express";
-
+import mongoose from "./database/index.js";
 const app=express();
-
+const db=mongoose.connection;
+db.on("error",console.error.bind(console,"connection error"))
+db.once("open",()=>{
+  console.log("database connected")
+})
 
 
 const PORT=5001;
