112dbb27baec37229c7084ce7abde2ad7304b369
mubashirshayan <shayanraees890@gmail.com>
2024-02-16 22:03:25 +0500

    today user scehama

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,6 +1,42 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
-const UserModel= new Schema();
+const UserModel= new Schema(
+{
+    username: {
+        type: String,
+        required: true,
+        unique: true
+      },
+      fullname: {
+        type: String,
+        required: true
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true
+      },
+      avatar: String,
+      coverImage: String,
+      password: {
+        type: String,
+        required: true
+      },
+      timestamp: {
+        type: Date,
+        default: Date.now
+      }
+}
+);
 const User= mongoose.model("User",UserModel
 )
 export default User;
+//id
+//user name
+//email
+//full name
+//avatar
+//cover image
+//password
+//refresh token
+//time stamp
