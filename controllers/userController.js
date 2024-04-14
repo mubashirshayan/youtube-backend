d4f8a215475faf632e22bc37c2d3fcb2337f7dff
mubashirshayan <shayanraees890@gmail.com>
2024-04-14 20:14:40 +0500

    add new function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,20 @@
 import mongoose from "mongoose"
 const register=(req,res)=>{
-    console.log("register user")
-    return res.status(200).send("hello register user")
+    try{
+        console.log("register user")
+        return res.status(200).send("hello register user")
+    }
+    catch{
+        
+    }
+}
+const login=(req,res)=>{
+    try{
+        console.log("register user")
+        return res.status(200).send("user login successfull")
+    }catch{
+
+    }
+  
 }
 export default register;
