64d6d9294a5f906dd2951b04d0daf81012cca083
mubashirshayan <shayanraees890@gmail.com>
2024-04-14 20:18:55 +0500

    add changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,11 @@
-import mongoose from "mongoose"
+//import mongoose from "mongoose"
 const register=(req,res)=>{
     try{
         console.log("register user")
         return res.status(200).send("hello register user")
     }
     catch{
-        
+        return res.status(200).send("hello register user")
     }
 }
 const login=(req,res)=>{
@@ -13,8 +13,8 @@ const login=(req,res)=>{
         console.log("register user")
         return res.status(200).send("user login successfull")
     }catch{
-
+        return res.status(200).send("hello register user")
     }
   
 }
-export default register;
+export { register,login};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { Router } from "express"
-import register from "../controllers/userController.js"
+import {register,login} from "../controllers/userController.js"
+
 const router=Router()
 console.log(router)
 router.route("/home").get(register);
